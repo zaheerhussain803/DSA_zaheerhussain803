f69d69be25d73e9d6b96efe8f14af50284e22572
zaheerhussain803 <syedzaheerhussain4@gmail.com>
2025-04-05 02:51:42 +0530

    Updating file: Chc_/TwoSum.java

---
+++ Chc_/TwoSum.java
@@ -0,0 +1,16 @@
+// Reverse String Solution
+import java.util.*;
+
+class Solution {
+    public void reverseString(char[] s) {
+        // Your code here
+        
+    }
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        solution.reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+}
