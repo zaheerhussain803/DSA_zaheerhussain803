86e1a7fa4e2884d324fa2bc6ad976f23b1c80bf6
zaheerhussain803 <syedzaheerhussain4@gmail.com>
2025-04-09 18:07:08 +0530

    Updating file: TwoSum1.java

---
+++ TwoSum1.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class TwoSumSolution {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            numMap.put(nums[i], i);
+        }
+
+        // Assuming one valid solution exists
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    // Test the method
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
